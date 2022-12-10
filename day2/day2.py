32eeff96ff0fdfdcdb7d2ec526dbb0f081a45a8b
rstacks <stacksrob910@tamu.edu>
2022-12-10 12:40:28 -0600

    added separate class for part 1 methods

--- day2/day2.py
+++ day2/day2.py
@@ -25,88 +25,102 @@
 # Determine the total score using this information.
 
 
-def get_weapon(char):
-    """Return weapon associated with provided character.
-    
-    Args:
-        char: single-character string representing a weapon
-    Returns:
-        corresponding weapon as string
-    Raises:
-        KeyError: provided char has no corresponding weapon
+class GametypeA:
     """
-    weapon_dict = {
-        "A":"rock", "B":"paper", "C":"scissors", 
-        "X":"rock", "Y":"paper", "Z":"scissors"
-    }
-    return weapon_dict[char]
+    Contains methods for running RPS rounds according to the
+    description of PART 1 of this challenge.
 
-
-def get_outcome(user_wep, opponent_wep):
-    """Determine outcome of an RPS round given contenders' weapons.
-
-    Args:
-        user_wep: string with player's chosen weapon
-        opponent_wep: string with opponent's chosen weapon
-    Returns:
-        "win", "draw", or "loss" depending on outcome for player
-    Raises:
-        KeyError: provided user_wep param is not an available weapon
+    Methods
+    -------
+    get_weapon(char)
+        Return weapon associated with provided character.
+    get_outcome(user_wep, opponent_wep)
+        Determine outcome of an RPS round given contenders' weapons.
+    get_round_score(user_wep, opponent_wep)
+        Return RPS round score given contenders' weapons.
+    get_total_score(round_scores)
+        Return total score given list of all individual round scores.
     """
-    win_combo_dict = {
-        "rock":"scissors", "paper":"rock", "scissors":"paper"
-    }
-    if win_combo_dict[user_wep] == opponent_wep:
-        return "win"
-    if user_wep == opponent_wep:
-        return "draw"
-    return "loss"
+    def get_weapon(self, char):
+        """Return weapon associated with provided character.
+        
+        Args:
+            char: single-character string representing a weapon
+        Returns:
+            corresponding weapon as string
+        Raises:
+            KeyError: provided char has no corresponding weapon
+        """
+        weapon_dict = {
+            "A":"rock", "B":"paper", "C":"scissors", 
+            "X":"rock", "Y":"paper", "Z":"scissors"
+        }
+        return weapon_dict[char]
 
+    def get_outcome(self, user_wep, opponent_wep):
+        """Determine outcome of an RPS round given contenders' weapons.
 
-def get_round_score(user_wep, opponent_wep):
-    """Return RPS round score given contenders' weapons.
+        Args:
+            user_wep: string with player's chosen weapon
+            opponent_wep: string with opponent's chosen weapon
+        Returns:
+            "win", "draw", or "loss" depending on outcome for player
+        Raises:
+            KeyError: provided user_wep param is not an available weapon
+        """
+        win_combo_dict = {
+            "rock":"scissors", "paper":"rock", "scissors":"paper"
+        }
+        if win_combo_dict[user_wep] == opponent_wep:
+            return "win"
+        if user_wep == opponent_wep:
+            return "draw"
+        return "loss"
 
-    Args:
-        user_wep: string with player's chosen weapon
-        opponent_wep: string with opponent's chosen weapon
-    Returns:
-        int score for player based on their chosen weapon and outcome
-        of round for them
-    Raises:
-        KeyError: provided user_wep param is not an available weapon
-    """
-    score_dict = {
-        "rock":1, "paper":2, "scissors":3,
-        "loss":0, "draw":3, "win":6
-    }
-    outcome = get_outcome(user_wep, opponent_wep)
-    return score_dict[user_wep] + score_dict[outcome]
+    def get_round_score(self, user_wep, opponent_wep):
+        """Return RPS round score given contenders' weapons.
 
+        Args:
+            user_wep: string with player's chosen weapon
+            opponent_wep: string with opponent's chosen weapon
+        Returns:
+            int score for player based on their chosen weapon and
+            outcome of round for them
+        Raises:
+            KeyError: provided user_wep param is not an available weapon
+        """
+        score_dict = {
+            "rock":1, "paper":2, "scissors":3,
+            "loss":0, "draw":3, "win":6
+        }
+        outcome = self.get_outcome(user_wep, opponent_wep)
+        return score_dict[user_wep] + score_dict[outcome]
 
-def get_total_score(round_scores):
-    """Return total score given list of all individual round scores.
-    
-    Args:
-        round_scores: iterable containing numeric values representing
-        RPS round scores
-    Returns:
-        numeric sum of all values in round_scores iterable
-    Raises:
-        TypeError: provided param contains non-numeric data
-    """
-    return sum(round_scores)
+    def get_total_score(self, round_scores):
+        """Return total score given list of all individual round scores.
+        
+        Args:
+            round_scores: iterable containing numeric values
+            representing RPS round scores
+        Returns:
+            numeric sum of all values in round_scores iterable
+        Raises:
+            TypeError: provided param contains non-numeric data
+        """
+        return sum(round_scores)
 
 
 # PART 1 Solution
 # Iterate through all lines in input file and compile round scores
+game1 = GametypeA()
 round_scores = []
 with open("./day2/day2_input.txt") as rounds:
     round = rounds.readline().strip()
     while round != "":
-        my_wep = get_weapon(round[-1])
-        elf_wep = get_weapon(round[0])
-        round_scores.append(get_round_score(my_wep, elf_wep))
+        my_wep = game1.get_weapon(round[-1])
+        elf_wep = game1.get_weapon(round[0])
+        round_scores.append(game1.get_round_score(my_wep, elf_wep))
         round = rounds.readline().strip()
 
 # PART 1 Output
-print(f"{get_total_score(round_scores)} is my total score")
+print(f"{game1.get_total_score(round_scores)} is my total score")
