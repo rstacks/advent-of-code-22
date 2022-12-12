76069da5ced532058f7b1ac3c9ab44fdf7cc9714
rstacks <stacksrob910@tamu.edu>
2022-12-11 23:18:05 -0600

    updated methods, completed challenge

--- day2/day2.py
+++ day2/day2.py
@@ -23,6 +23,15 @@
 #       Y: paper
 #       Z: scissors
 # Determine the total score using this information.
+#
+# PART 2:
+# The first letter still represents the opponent's weapon, but the
+# second letter now represents the outcome of the round for us:
+#       X: loss
+#       Y: draw
+#       Z: win
+# Points are assigned in the same way as PART 1. Determine the total
+# score using this information.
 
 
 class GametypeA:
@@ -32,8 +41,8 @@ class GametypeA:
 
     Methods
     -------
-    get_weapon(char)
-        Return weapon associated with provided character.
+    get_weapons(user_char, opponent_char)
+        Returns tuple of weapons from chars as (user_wep, opponent_wep).
     get_outcome(user_wep, opponent_wep)
         Determine outcome of an RPS round given contenders' weapons.
     get_round_score(user_wep, opponent_wep)
@@ -42,27 +51,28 @@ class GametypeA:
         Return total score given list of all individual round scores.
     """
     
-    def get_weapon(self, char):
+    def get_weapons(self, user_char, opponent_char):
         """
-        Return weapon associated with provided character.
+        Returns tuple of weapons from chars as (user_wep, opponent_wep).
         
         Args
         ----
-        char: single-character string representing a weapon
+        user_char: single-character string for the player's weapon
+        opponent_char: single-character string for opponent's weapon
         
         Returns
         -------
-        corresponding weapon as string
+        corresponding weapons as strings
         
         Raises
         ------
-        KeyError: provided char has no corresponding weapon
+        KeyError: provided char(s) has no corresponding weapon
         """
         weapon_dict = {
             "A":"rock", "B":"paper", "C":"scissors", 
             "X":"rock", "Y":"paper", "Z":"scissors"
         }
-        return weapon_dict[char]
+        return (weapon_dict[user_char], weapon_dict[opponent_char])
 
     def get_outcome(self, user_wep, opponent_wep):
         """
@@ -135,18 +145,43 @@ class GametypeA:
         return sum(round_scores)
 
 
-# new get_weapons(my_char, elf_char) method
-# returns tuple of strings for me and the elf
-# calls superclass get_weapon for elf normally, will call
-# get_weapon for me depending on win/loss relative to elf's game
-
 class GametypeB(GametypeA):
+    """
+    Extends GametypeA. Overrides get_weapons(user_char, opponent_char)
+    method to reflect the new description of RPS rounds in PART 2 of
+    this challenge.
+
+    Methods
+    -------
+    get_weapons(user_char, opponent_char)
+        Returns tuple of weapons from chars as (user_wep, opponent_wep).
+    """
+
     def get_weapons(self, user_char, opponent_char):
+        """
+        Returns tuple of weapons from chars as (user_wep, opponent_wep).
+        
+        Args
+        ----
+        user_char: single-character string for round outcome
+        opponent_char: single-character string for opponent's weapon
+        
+        Returns
+        -------
+        corresponding weapons as strings
+        
+        Raises
+        ------
+        KeyError: provided char(s) has no matching weapon or outcome
+        """
+        # Determine correct round outcome given user_char
         outcome_dict = {"X":"loss", "Y":"draw", "Z":"win"}
         correct_outcome = outcome_dict[user_char]
+        # Assign opponent weapon using superclass method
+        opponent_wep = super().get_weapons(user_char, opponent_char)[1]
+        # Select user_wep that yields the correct round outcome
         wep_list = ["rock", "paper", "scissors"]
         user_wep = wep_list[0]
-        opponent_wep = self.get_weapon(opponent_char)
         trial_outcome = self.get_outcome(user_wep, opponent_wep)
         while trial_outcome != correct_outcome:
             wep_list = wep_list[1:]
@@ -155,17 +190,38 @@ class GametypeB(GametypeA):
         return (user_wep, opponent_wep)
 
 
+def play_game(game):
+    """
+    Prints total score after running all rounds for provided gametype.
+
+    Args
+    ----
+    game: GametypeA or GametypeB object to calculate score for
+    
+    Returns
+    -------
+    None
+    
+    Raises
+    ------
+    AttributeError: param is not an object of GametypeA
+    """
+    round_scores = []
+    with open("./day2/day2_input.txt") as rounds:
+        round = rounds.readline().strip()
+        while round != "":
+            my_wep, elf_wep = game.get_weapons(round[-1], round[0])
+            round_scores.append(game.get_round_score(my_wep, elf_wep))
+            round = rounds.readline().strip()
+    print(f"{game.get_total_score(round_scores)} is my total score")
+
 # PART 1 Solution
 # Iterate through all lines in input file and compile round scores
 game1 = GametypeA()
-round_scores = []
-with open("./day2/day2_input.txt") as rounds:
-    round = rounds.readline().strip()
-    while round != "":
-        my_wep = game1.get_weapon(round[-1])
-        elf_wep = game1.get_weapon(round[0])
-        round_scores.append(game1.get_round_score(my_wep, elf_wep))
-        round = rounds.readline().strip()
+print("Part 1:", end=" ")
+play_game(game1)
 
-# PART 1 Output
-print(f"{game1.get_total_score(round_scores)} is my total score")
+# PART 2 Solution
+game2 = GametypeB()
+print("Part 2:", end=" ")
+play_game(game2)
